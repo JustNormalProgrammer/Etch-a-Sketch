884f4cf4ef83b2d0a3b29c11abca80ebb36808f9
JustNormalProgrammer <testkonto1290@gmail.com>
2024-09-18 11:03:41 +0200

    Add more features and refactor code in script.js.
    User can now increase the opacity of an element of
    the grid when mouseenter event is fired on said
    element. Each time the event is fired the
    background color of an element is randomized.
    Said event is only executing when LMB is actively
    being pressed.
    Improve style on the index page.

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
-
-
-
-
+let isMouseDown = false;
+container.addEventListener('mousedown', () => isMouseDown = true);
+container.addEventListener('mouseup', () => isMouseDown = false);
 function makeGrid(size) {
-    for(let i = 0; i < size*size; i++){
+    let width = parseInt(container.offsetHeight) / size;
+    for (let i = 0; i < size * size; i++) {
         const div = document.createElement('div');
         div.classList.add('square');
-        let width = 900/size;
         div.style.width = `${width}px`;
         div.style.height = `${width}px`;
-        div.addEventListener('mouseenter', () =>{
-            div.classList.add('color');
+        div.style.opacity = 0;
+        console.log(div.style.opacity);
+        div.addEventListener('mouseenter', () => {
+            if (isMouseDown) {
+                let [r, g, b] = getRandomColor();
+                div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+                div.style.opacity = parseFloat(div.style.opacity) + 0.1;
+                console.log(div.style.opacity)
+            }
         })
         container.appendChild(div);
-    }      
-}
-function reset(){
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
     }
 }
-button.addEventListener('click', ()=>{
+function reset() {
+    container.innerHTML = '';
+}
+function getRandomColor() {
+    let r = Math.floor((Math.random() * 256));
+    let g = Math.floor((Math.random() * 256));
+    let b = Math.floor((Math.random() * 256));
+    return [r, g, b];
+}
+button.addEventListener('click', () => {
     let size;
-    do{
+    do {
         size = prompt("Select size: 1-100", 64);
-        if(size === null) break;
-        
-    } while(size > 100 || size < 1);
-    reset();
-    makeGrid(size);
+        if (size === null) break;
+        reset();
+        makeGrid(size);
+    } while (size > 100 || size < 1);
 
 })
